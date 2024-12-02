cef34d3bd348aec266f2841c5d9240d936aa4a1b
mohammedfaizaansharief1 <mohdfaizaansharief@gmail.com>
2024-12-03 05:13:31 +0530

    completed week_9 with the correct output

---
+++ week_9/a.php
@@ -0,0 +1,19 @@
+<!-- Week 9: Create a PHP application program for authenticating users for the above program using sessions. -->
+
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+?>
+
+</body>
+</html>
